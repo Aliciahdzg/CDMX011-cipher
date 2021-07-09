65edd29967f9142f2115adcf7ba05ae0b12b2961
Aliciahdzg <hernndez.alicia4@gmail.com>
2021-07-08 23:12:40 -0500

    funcionalidad basica lista

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,56 @@
 const cipher = {
-  // ...
-};
+    encode: (offset, string) => {
+        let mensajeMayus = string.toUpperCase();
+        let resultadoCifrado = "";
+        /*ciclo for para iterar por cada letra y sacar el codigo ASCII donde i es igual al indice donde va a iniciar
+        el metodo length devuelve el número de elementos de mi lista de caracteres de mensaje
+        y lo hara mientras i sea menor que mensajeMayus.length, al finalizar la iteracion a i le suma 1 al indice para cambiar el varlor*/
+        for (let i = 0; i < mensajeMayus.length; i++) {
+            let letraASCII = mensajeMayus.charCodeAt(i); //variable que guarda el numero ASCII de cada letra que se esta iterando
+            let cifrar = ((letraASCII - 65 + offset) % 26) + 65; //Formula del cifrado cesar 
+            let Cifrado = String.fromCharCode(cifrar);
+
+            resultadoCifrado += Cifrado;
+
+
+            /*let resultado = "";
+            let letraCifrar = 0;
+            for (let i = 0; i < mensajeEscrito.length; i++) {
+                let letraASCII = mensajeEscrito.charCodeAt(i);
+                if ((letraASCII >= 65) && (letraASCII < 91)) {
+                    letraCifrar = ((letraASCII - 65 + desplazamiento) % 26) + 65;
+                    console.log(letraASCII)
+                    console.log(letraCifrar)
+                }
+                let nuevaCadena = String.fromCharCode(letraCifrar);
+                resultado += nuevaCadena;
+                return resultado;*/
+
+        }
+
+        return resultadoCifrado
+    },
+
+    decode: (offset, string) => {
+        let mensajeDescifrado = "";
+        for (let i = 0; i < string.length; i++) {
+            let letraASCII = string.charCodeAt(i);
+            console.log(letraASCII)
+            let descifrar = ((letraASCII + 65 - offset) % 26) + 65;
+            let descifrado = String.fromCharCode(descifrar);
+
+            mensajeDescifrado += descifrado;
+
+        }
+
+        return mensajeDescifrado
+    }
+
+
+}
+
+
+
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-import cipher from './cipher.js';
+import cipher from './cipher.js'
+//console.log(cipher);
 
-console.log(cipher);
+function mostrarMensaje() {
+    let mensajeEscrito = document.getElementById("mensajeEscrito").value;
+    let desplazamiento = document.getElementById("desplazamiento").value;
+    desplazamiento = parseInt(desplazamiento);
+    document.getElementById("mensajeCifrado").value = cipher.encode(desplazamiento, mensajeEscrito);
+}
+
+document.getElementById("cifrarBTN").addEventListener("click", mostrarMensaje);
+
+function mostrarMensajeDescifrado() {
+    let mensajeEscrito = document.getElementById("mensajeEscrito").value;
+    let desplazamiento = document.getElementById("desplazamiento").value;
+    desplazamiento = parseInt(desplazamiento);
+    document.getElementById("mensajeCifrado").value = cipher.decode(desplazamiento, mensajeEscrito)
+}
+
+document.getElementById("descifrarBTN").addEventListener("click", mostrarMensajeDescifrado)
+
+function borrar() {
+    let mensajeEscrito = document.getElementById("mensajeEscrito").value;
+    let desplazamiento = document.getElementById("desplazamiento").value;
+    let mensajeCipher = document.getElementById("mensajeCifrado").value;
+
+    document.getElementById("mensajeEscrito").value = ""
+    document.getElementById("mensajeCifrado").value = ""
+    document.getElementById("desplazamiento").value = 1
+
+}
+
+document.getElementById("borrarBTN").addEventListener("click", borrar)
